ae4e7db198d32a1e97368d8612f1c4e147d9b827
John <johnohannes53@gmail.com>
2026-08-20 22:14:03 +0300

    fixed connections

--- server/connection.php
+++ server/connection.php
@@ -1,25 +1,14 @@
 <?php
 
-// CORS headers - allow the frontend to call these endpoints from any origin
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
-header('Access-Control-Allow-Headers: Content-Type');
-
-// Handle preflight OPTIONS requests
-if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-    http_response_code(204);
-    exit;
-}
-
 $DB_HOST = 'localhost';
-$DB_NAME = 'lost_in_translation';
+$DB_NAME = 'leaf';
 $DB_USER = 'root';
 $DB_PASS = '';   
 
-$conn = mysqli_connect($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME);
+$mysql = new mysqli($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME);
 
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
+if (!$mysql) {
+    die("Connection failed: " . $mysql->connect_error);
 }
 
-mysqli_set_charset($conn, 'utf8mb4');
+?>
